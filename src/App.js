d6cbb8d57bd1af962ffa964b20efc6639758752a
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-22 19:47:10 -0300

    modulates the content of App.js
      * remove the content of App.js
        transform it into a component to put it back

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { Router, Route } from 'react-router'
 import './App.css';
+
+const Example1 = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Counter />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <span>
+        <span>Learn </span>
+        <a
+          className="App-link"
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+        <span>, </span>
+        <a
+          className="App-link"
+          href="https://redux.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Redux
+        </a>
+        <span>, </span>
+        <a
+          className="App-link"
+          href="https://redux-toolkit.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Redux Toolkit
+        </a>
+        ,<span> and </span>
+        <a
+          className="App-link"
+          href="https://react-redux.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Redux
+        </a>
+      </span>
+    </header>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <Example1/>
+  )
 }
 
 export default App;
