2289151b75bcf766d901139e6751f13da51fdabc
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-06-25 17:04:45 -0300

    Formik library added

--- src/backoffice/ComponentFormCategories.js
+++ src/backoffice/ComponentFormCategories.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+import { Formik } from 'formik'
+import { Container, Center } from "@chakra-ui/react"
+
 const ComponentFormCategories = (props) => {
     return (
         <div>
-            {props.match.params.id 
-            ? <h1>Formulario de EDICION</h1>
-            : <h1>Formulario de CREACION</h1>}
-                        
+            <Container marginTop="1.5%">
+                <Center d="flex" flexDirection="column">
+                    {props.match.params.id 
+                    ? <h1>Formulario de EDICION</h1>
+                    : <h1>Formulario de CREACION</h1>}
+                    
+                </Center>
+            </Container>
         </div>
     )
 }
