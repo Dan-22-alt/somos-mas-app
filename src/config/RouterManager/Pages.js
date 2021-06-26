8bc6c1d51e2d70650868888cb4f5965bba5b7431
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-26 17:35:46 -0300

    Add a page
      * it's path is '/backoffice/categories'

--- src/config/RouterManager/Pages.js
+++ src/config/RouterManager/Pages.js
@@ -1,7 +1,9 @@
 import { Example1 } from '../../pages/Example1'
 import { Example2 } from '../../pages/Example2'
+import { CategoriesPage } from '../../pages/CategoriesPage'
 import ComponentScreenListOfNews from '../../backoffice/ComponentScreenListOfNews'
 import ComponentCreateNews from '../../backoffice/ComponentCreateNews'
+
 
 /*
   component = Ahi iria la pagina
@@ -23,5 +25,6 @@ export const Pages = [
   new Page(Example1, '/'),
   new Page(Example2, '/example2'),
   new Page(ComponentScreenListOfNews, '/backoffice/news'),
-  new Page(ComponentCreateNews, '/backoffice/news/create')
+  new Page(ComponentCreateNews, '/backoffice/news/create'),
+  new Page(CategoriesPage, '/backoffice/categories')
 ]
---
+++ src/pages/CategoriesPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const CategoriesPage = () => (
+  <div className="App">
+    hello Dear
+  </div>
+)
