d3e95774debb37233cd004e1ca312f630ba9f438
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-26 21:28:38 -0300

    emule request
      * fetch request to api/categories

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, SimpleGrid, WrapItem, Center, Wrap } from "@chakra-ui/react"
 
-export const CategoriesPage = () => (
-  <div className="App">
-    hello Dear
-  </div>
-)
+
+
+export const CategoriesPage = () => {
+  const [categories, setCategories] = useState({
+    loading: true,
+    res: null,
+    error: ''
+  })
+
+  useEffect(() => {
+    fetch('http://ongapi.alkemy.org/api/categories')
+      .then(r => r.json())
+      .then(r => setCategories(prevState =>
+        ({...prevState, res: r.data, loading: false})))
+      .catch( error => setCategories(prevState =>
+        ({...prevState, error, loading: false}) ))
+  }, [])
+
+  console.log()
+  return (
+    <Wrap>
+    </Wrap>
+  )
+}
+
+//      {categories.map( category => <Category />)}
