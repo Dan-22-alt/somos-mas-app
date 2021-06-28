7659bbd4cb39ca49d37028fd7b8944b02d6d64c7
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-28 13:48:26 -0300

    Add a Link to 'backoffice/categories/create'

--- src/backoffice/categories/Category.js
+++ src/backoffice/categories/Category.js
@@ -6,7 +6,12 @@ export const Category = ({name, created_at, id, deleteItem}) => {
   return(
     <Tr>
       <Td px={[1, 5, 10, 10]}>{name}</Td>
-      <Td textAlign='center' px={[1, 5, 10, 10]}>{created_at.replace(/T.*/, '')}</Td>
+      <Td
+        textAlign='center'
+        px={[1, 5, 10, 10]}
+      >
+        {created_at.replace(/T.*/, '')}
+      </Td>
       <Td px={[1, 5, 10, 10]}
         display='flex'
         flexWrap='wrap'
