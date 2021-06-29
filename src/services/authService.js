3496c5825a9779aa6ea6a654460e8b23e5f84bd9
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-06-29 14:16:10 -0300

    New alerts for Login added

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import axios from 'axios'
 
+
 const login = (values) => {
     axios.post(`${process.env.REACT_APP_API_LOGIN}`, values)
         .then(res => {
-            alert('success')
             console.log(res)
+            return true
         })
         .catch(err => {
-            alert('error')
             console.log(err)
+            return true
         })
 }
+
 
 export {login}
