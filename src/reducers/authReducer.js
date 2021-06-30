a117cc74bc2e1a2a7277e72522912b72e80fca50
aibbya <aibbyale@gmail.com>
2021-06-30 18:26:01 -0300

    condicionar para realizar el dispacht segun la respuesta del login

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -14,12 +14,15 @@ const authSlice = createSlice({
   initialState: authState,
   reducers: {
     loginSuccess: (state, action) => {
+      state.status = null
       state.token = action.payload;
       state.status = 'success'
     },
     loginFailed: (state, action) => {
+      state.status = null
       state.error = action.payload;
       state.status = 'error'
+
     },
   },
 });
@@ -28,18 +31,21 @@ export const { loginSuccess, loginFailed } = authSlice.actions;
 
 export const selectAuth = state => state.auth.status;
 
+
 export const authLog = values => dispatch => {
   axios.post(`${process.env.REACT_APP_API_LOGIN}`, values)
     .then(res => {
-      console.log(res)
+      console.log(res.data)
       // const token = res.data.token
       // localStorage.setItem('token', token)
       // return 
-      if (res.data.token){
-        dispatch(loginSuccess(values));
-      }
+      
       if (res.data.error){
+        console.log('res error');
         dispatch(loginFailed(values));
+      } else if (res.data.data.token){
+        console.log('res tock');
+        dispatch(loginSuccess(values));
       }
     })
     .catch(err => {
