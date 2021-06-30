5b99158b53c916ff6af71f80a80a8e4a123c9912
Francoveliz <francoleandroveliz@gmail.com>
2021-06-30 02:20:03 -0300

    added console.log that prints user sesion status

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Route } from './config/RouterManager/Route'
-import { Pages } from './config/RouterManager/Pages'
-import { ApiService } from './services/ApiService';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route } from "./config/RouterManager/Route";
+import { Pages } from "./config/RouterManager/Pages";
+import { ApiService } from "./services/ApiService";
+import userIsLogged from "./features/auth/userIsLogged";
+import "./App.css";
 
 function App() {
-  const example = ApiService({})
+	useEffect(() => {
+		console.log(
+			`${
+				userIsLogged()
+					? "El usuario esta autenticado"
+					: "El usuario no esta autenticado"
+			}`
+		);
+	}, []);
 
-  return (
-    <Router>
-      <Switch>
-        {Pages.map(page => <Route {...page} />)}
-      </Switch>
-    </Router>
-  )
+	return (
+		<Router>
+			<Switch>
+				{Pages.map(page => (
+					<Route {...page} />
+				))}
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
