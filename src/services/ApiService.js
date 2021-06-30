cfb4e0fb74486964fcc11d6a265d045ae78dd99e
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-29 23:01:34 -0300

    add react-icons and fix fetch
      * fix issus with .env in the master

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,10 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Api_Alkemy = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
-})
-
 const init = {
   res: null,
   loading: true,
