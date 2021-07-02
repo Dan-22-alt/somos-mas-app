d5cfafa87fd6bf6c9001e802b55e4b2e92f60333
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-02 18:34:19 -0300

    Conflict resolution with package-lock

--- src/features/auth/formLogin.js
+++ src/features/auth/formLogin.js
@@ -1,127 +1,39 @@
-import React from 'react';
-import {
-    Flex,
-    Heading,
-    Input,
-    Button,
-    Stack,
-    Box,
-    Avatar,
-    FormControl,
-    FormLabel,
-    Alert,
-    AlertIcon,
-} from '@chakra-ui/react';
-import { useFormik } from "formik";
-import * as Yup from 'yup';
-import { login } from '../../services/authService'
 
-const FormLogin = () => {
+import React, { useEffect } from 'react';
+import FormLogin from "../features/auth/formLogin"
+import { useSelector } from 'react-redux';
+import { selectAuth } from '../reducers/authReducer';
+import { useToast } from '@chakra-ui/react';
+import AvoidAuthRedundancies from "../features/auth/AvoidAuthRedundancies";
 
-    // Formulario y validación con formik y Yup
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
-        },
-        validationSchema: Yup.object({
-            email: Yup.string()
-                .email('El email no es válido')
-                .required('El Email es Obligatorio'),
-            password: Yup.string()
-                .required('Ingrese su contraseña')
-                .min(6, 'El password debe contener al menos 6 caracteres')
-        }),
-        onSubmit: valores => {
-            const values = {
-                email: valores.email,
-                password: valores.password
-            };
-            login(values)
-                        
+const Login = () => {
+
+    const log = useSelector(selectAuth);
+    const toast = useToast();
+
+    useEffect(() => {
+        if (log.state === 'success') {
+            toast({
+                description: "Bienvenido!",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+            });
+        } else if (log.state === 'error') {
+            toast({
+                description: "Email o contraseña incorrecta",
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            });
         }
     });
 
-
     return (
-        <Flex
-            flexDirection="column"
-            width="100wh"
-            height="100vh"
-            backgroundColor="gray.200"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Stack
-                flexDir="column"
-                mb="2"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Avatar bg="teal.500" />
-                <Heading color="teal.400">Login</Heading>
-                <Box minW={{ base: "90%", md: "468px" }}>
-                    <form
-                        onSubmit={formik.handleSubmit}
-                    >
-                        <Stack
-                            spacing={4}
-                            p="1rem"
-                            backgroundColor="whiteAlpha.900"
-                            boxShadow="md"
-                        >
-                            <FormControl mt={2}>
-                                {formik.touched.email && formik.errors.email ? (
-                                    <Alert justifyContent="center" status="error">
-                                        <AlertIcon />
-                                        {formik.errors.email}
-                                    </Alert>
-                                ) : null}
-                                <FormLabel>Email</FormLabel>
-                                <Input
-                                    type="email"
-                                    placeholder="Mquinola@maquinola.com"
-                                    id="email"
-                                    value={formik.values.email}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                />
-                            </FormControl>
-
-                            <FormControl mt={2}>
-                                {formik.touched.password && formik.errors.password ? (
-                                    <Alert justifyContent="center" status="error">
-                                        <AlertIcon />
-                                        {formik.errors.password}
-                                    </Alert>
-                                ) : null}
-                                <FormLabel>Password</FormLabel>
-                                <Input
-                                    type="password"
-                                    placeholder="*******"
-                                    id="password"
-                                    value={formik.values.password}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                />
-
-                            </FormControl>
-                            <Button
-                                borderRadius={0}
-                                type="submit"
-                                variant="solid"
-                                colorScheme="teal"
-                                width="full"
-                            >
-                                Login
-                            </Button>
-                        </Stack>
-                    </form>
-                </Box>
-            </Stack>
-        </Flex>
-
+        <AvoidAuthRedundancies>
+			<FormLogin />
+		</AvoidAuthRedundancies>
     );
 }
 
-export default FormLogin;
+export default Login;
