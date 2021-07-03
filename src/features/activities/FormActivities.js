63e43eebac2ce89f1dac149da74436fd5b1ff1a1
aibbya <aibbyale@gmail.com>
2021-07-02 23:27:28 -0300

    desarollo de logica y comentarios para que pueda reutilizarse

--- src/features/activities/FormActivities.js
+++ src/features/activities/FormActivities.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Formik } from "formik";
+import "./stylesForm.css";
 import { useParams } from 'react-router-dom';
 import {
     Flex,
@@ -8,141 +9,162 @@ import {
     Button,
     Stack,
     Box,
-    Image,
     FormControl,
     FormLabel,
-    Alert,
-    AlertIcon,
-    Textarea,
+    useToast,
 } from '@chakra-ui/react';
-import { BsFileText } from "react-icons/bs";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import axios from 'axios'
 
 
-const FormActivities = () => {
+const FormActivities = ({ data }) => {
 
-    const { id } = useParams()
-    const [modeForm, setMode] = useState()
+    // Posible mejora: que tome la data por medio del parametro
+    // const { id } = useParams()
+
+    const toast = useToast();
     const [description, setDescription] = useState('')
 
+    // si recibe data por medio props toma los valores que vienen alli, si no es un formulario vacio
+    // los titulos, notificaciones, botones y Acciones se adaptan segun la funcion que vaya a cumplir
     const initialValues = {
-        name: '',
-        description: '',
-        image: '',
+        name: data ? data.name : '',
+        description: data ? data.description : '',
+        image: data ? data.image : '',
     }
 
-    const enviarData = (even) => {
-        even.preventDefault()
-        // console.log(({...values , description: description }))
-        console.log("gsdfsd");
+    const enviarData = (values, actions) => {
+            axios({ method: data ? 'PUT' : 'POST' ,  
+                    url: data ? `${process.env.REACT_APP_API_ACTIVITY}/` + data.id : `${process.env.REACT_APP_API_ACTIVITY}` ,
+                    data: { ...values, description: description } })
+                .then(res => {
+                    console.log(res)
+                    toast({
+                        description: data ? "Editado!" : "Actividad creada!",
+                        status: "success",
+                        duration: 2000,
+                        isClosable: true,
+                    });
+                    actions.resetForm()
+                })
+                .catch(err => {
+                    console.log(err)
+                    toast({
+                        description: "Error del servidor!",
+                        status: "error",
+                        duration: 2000,
+                        isClosable: true,
+                    });
 
+                })
     }
 
 
     return (
 
-        <Formik initialValues={initialValues} onSubmit={(values, actions) => {
-            setTimeout(() => {
-                alert(JSON.stringify(({...values, description: description}), null, 2));
-                actions.setSubmitting(false);
-            }, 1000);
-        }}>
-            {props => (
-                <Flex
-                    flexDirection="column"
-                    width="100wh"
-                    height="100vh"
-                    backgroundColor="gray.200"
-                    justifyContent="center"
-                    alignItems="center"
-                >
+        <div  className="SeccFormActivities" >
 
-                    <Stack
-                        flexDir="column"
-                        mb="2"
+            <Formik initialValues={initialValues} onSubmit={(values, actions) => { enviarData(values, actions) }} >
+                {props => (
+                    <Flex
+                        flexDirection="column"
+                        width="100wh"
+                        height="100vh"
+                        backgroundColor="gray.200"
                         justifyContent="center"
                         alignItems="center"
                     >
-                        <Heading color="teal.400">Formulario de Actividades</Heading>
-                        <Box minW={{ base: "90%", md: "468px" }}>
-                            <form onSubmit={props.handleSubmit}>
-                                <Stack
-                                    spacing={4}
-                                    p="1rem"
-                                    backgroundColor="whiteAlpha.900"
-                                    boxShadow="md"
-                                >
-                                    <FormControl mt={2}>
-                                        <FormLabel>Nombre</FormLabel>
-                                        <Input
-                                            type="text"
-                                            onChange={props.handleChange}
-                                            onBlur={props.handleBlur}
-                                            value={props.values.name}
-                                            name="name"
-                                            id="name"
-                                        />
-                                    </FormControl>
 
-                                    <FormControl mt={2}>
-                                        <FormLabel>Descripción</FormLabel>
-                                        <CKEditor
-                                            editor={ClassicEditor}
-                                            data="<p>Hello from CKEditor 5!</p>"
-                                            onReady={editor => {
-                                                // You can store the "editor" and use when it is needed.
-                                                console.log('Editor is ready to use!', editor);
-                                            }}
-                                            onChange={(event, editor) => {
-                                                const data = editor.getData();
-                                                setDescription(data)
-                                                console.log(description)
-                                                // console.log({ event, editor, data });
-                                            }}
-                                            onBlur={(event, editor) => {
-                                                console.log('Blur.', editor);
-                                            }}
-                                            onFocus={(event, editor) => {
-                                                console.log('Focus.', editor);
-                                            }}
-                                        />
+                        <Stack
+                            flexDir="column"
+                            mb="2"
+                            justifyContent="center"
+                            alignItems="center"
+                            maxW="md"
+                        >
+                            <Heading color="teal.400">{data ? "Edita la actividad" : "Crea una actividad"}</Heading>
+                            <Box minW={{ base: "90%", md: "468px" }}>
+                                <form onSubmit={props.handleSubmit} className="formActivities">
+                                    <Stack
+                                        spacing={4}
+                                        p="1rem"
+                                        backgroundColor="whiteAlpha.900"
+                                        boxShadow="md"
+                                    >
+                                        <FormControl mt={2}>
+                                            <FormLabel>Nombre</FormLabel>
+                                            <Input
+                                                type="text"
+                                                onChange={props.handleChange}
+                                                onBlur={props.handleBlur}
+                                                value={props.values.name}
+                                                variant="flushed"
+                                                name="name"
+                                                id="name"
+                                            />
+                                        </FormControl>
 
+                                        <FormControl mt={2}>
+                                            <FormLabel>Descripción</FormLabel>
+                                            <CKEditor
+                                                editor={ClassicEditor}
+                                                data={data ? data.description : ''}
+                                                onReady={editor => {
+                                                    // You can store the "editor" and use when it is needed.
+                                                    
+                                                }}
+                                                onChange={(event, editor) => {
+                                                    const data = editor.getData();
+                                                    setDescription(data)
+                                                    // console.log({ event, editor, data });
+                                                }}
+                                                onBlur={(event, editor) => {
+                                                    // console.log('Blur.', editor);
+                                                }}
+                                                onFocus={(event, editor) => {
+                                                    // console.log('Focus.', editor);
+                                                }}
+                                            /> 
 
-                                    </FormControl>
-                                    <FormControl mt={2}>
-                                        <FormLabel>Imagen</FormLabel>
-                                        <Input
-                                            type="file"
-                                            onChange={props.handleChange}
-                                            onBlur={props.handleBlur}
-                                            value={props.values.file}
-                                            name="image"
-                                            id="image"
-                                        />
-                                    </FormControl>
-                                    {/* {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+                                        </FormControl>
+                                        <FormControl mt={2}>
+                                            <FormLabel>Imagen</FormLabel>
+                                            <Input
+                                                type="file"
+                                                onChange={props.handleChange}
+                                                onBlur={props.handleBlur}
+                                                value={props.values.file}
+                                                variant="flushed"
+                                                name="image"
+                                                id="image">
+                                                
+                                            </Input>
+
+                                        </FormControl>
+                                        {/* {props.errors.name && <div id="feedback">{props.errors.name}</div>}
                                     <button type="submit">Submit</button> */}
 
-                                    <Button
-                                        borderRadius={0}
-                                        type="submit"
-                                        variant="solid"
-                                        colorScheme="teal"
-                                        width="full"
-                                    >
-                                        Enviar
-                                    </Button>
-                                </Stack>
-                            </form>
-                        </Box>
-                    </Stack>
+                                        <Button
+                                            borderRadius={0}
+                                            type="submit"
+                                            variant="solid"
+                                            colorScheme="teal"
+                                            width="full"
+                                        >
+                                            {data ? "Editar" : "Crear"}
+                                        </Button>
+                                    </Stack>
+                                </form>
+                            </Box>
+                        </Stack>
 
 
 
-                </Flex>
-            )}
-        </Formik>
+                    </Flex>
+                )}
+            </Formik>
+        </div>
 
     );
 }
