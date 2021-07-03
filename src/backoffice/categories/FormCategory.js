c7873435fab44d568bd57e8ce5ed67aab8ebc29b
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-02 22:03:28 -0300

    Functions post and put added

--- src/backoffice/categories/FormCategory.js
+++ src/backoffice/categories/FormCategory.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Flex,
     Heading,
@@ -13,8 +13,130 @@ import {
     Image,
     useToast
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-const FormCategory = () => {
+const FormCategory = ({cate}) => {
+
+    const toast = useToast();
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    const [newCategory, setNewCategory] = useState({
+        id: '',
+        name: '',
+        image: '',
+        description: ''
+    });
+
+    const { image } = newCategory;
+
+    const [selectetdFile, setSelectedFile] = useState([]);
+    const [imagenes, setImage] = useState();
+    const [preview, setPreview] = useState();
+
+    useEffect(() => {
+        encodeFileBase64(selectetdFile[0])
+
+        if (imagenes) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result);
+            };
+            reader.readAsDataURL(imagenes);
+        } else if (cate) {
+            setPreview(cate.image);
+        } else {
+            setPreview("/images/profileFace.svg")
+        }
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1500)
+    }, [selectetdFile, imagenes])
+
+    const encodeFileBase64 = (file) => {
+        var reader = new FileReader();
+        if (file) {
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                var Base64 = reader.result;
+
+                setNewCategory({
+                    ...cate,
+                    image: Base64,
+                });
+            };
+            reader.onerror = (error) => {
+                console.log("error: ", error);
+            };
+        }
+    };
+
+    const onFileChange = (e) => {
+
+        setSelectedFile(e.target.files);
+        setImage(e.target.files[0]);
+
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            name: cate ? cate.name : "",
+            description: cate ? cate.description : ""
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('Name requerido')
+                .min(6, 'El nombre debe contener al menos 6 caracteres'),
+            description: Yup.string()
+                .required('Description requerido')
+                .min(6, 'La descripcion debe contener al menos 6 caracteres')
+        }),
+        onSubmit: valores => {
+            const values = {
+                name: valores.name,
+                description: valores.description,
+                image: newCategory.image
+            };
+            console.log(values)
+            if (cate) {
+                axios
+                    .put(`${process.env.REACT_APP_API_CATEGORY}/${cate.id}`, values)
+                    .then(res => {
+                        console.log(res)
+                        if (res.data.error) {
+                            alert('Debes proporcionar una imagen')
+                        } else {
+                            toast({
+                                title: "Categoria editada",
+                                status: "success",
+                                duration: 2000,
+                                isClosable: true,
+                            });
+                        }
+                    })
+            } else {
+                axios
+                    .post(`${process.env.REACT_APP_API_CATEGORY}`, values)
+                    .then(res => {
+                        console.log(res)
+                        if (res.data.error) {
+                            alert('Debes proporcionar una imagen')
+                        } else {
+                            toast({
+                                title: "Categoria creada",
+                                status: "success",
+                                duration: 2000,
+                                isClosable: true,
+                            });
+                        }
+                    })
+
+            }
+        }
+    });
+
     return (
         <Flex
             flexDirection="column"
@@ -24,20 +146,22 @@ const FormCategory = () => {
             justifyContent="center"
             alignItems="center"
         >
-                 <></>
-                 <Stack
+            {isLoading
+                ? <></>
+                : <Stack
                     flexDir="column"
                     mb="2"
                     justifyContent="center"
                     alignItems="center"
                 >
-                    {test
-                        ? <Heading color="teal.400">Editar testimonio</Heading>
-                        : <Heading color="teal.400">Nuevo testimonio</Heading>
+                    {cate
+                        ? <Heading color="teal.400">Editar categoria</Heading>
+                        : <Heading color="teal.400">Nueva categoria</Heading>
                     }
                     <Box minW={{ base: "90%", md: "468px" }}>
 
                         <form
+                            onSubmit={formik.handleSubmit}
                         >
                             <Stack
                                 spacing={4}
@@ -50,7 +174,16 @@ const FormCategory = () => {
                                     <Input
                                         type="text"
                                         id="name"
+                                        value={formik.values.name}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
                                     />
+                                    {formik.touched.name && formik.errors.name ? (
+                                        <Alert justifyContent="center" status="error">
+                                            <AlertIcon />
+                                            {formik.errors.name}
+                                        </Alert>
+                                    ) : null}
                                 </FormControl>
 
                                 <FormControl id="image">
@@ -58,7 +191,10 @@ const FormCategory = () => {
                                     <Box d="flex">
                                         <input
                                             type="file"
+                                            name={image}
+                                            onChange={onFileChange}
                                         />
+                                        <Image src={preview} alt="imagen" width="100px" borderRadius="10%" m="auto" />
                                     </Box>
                                 </FormControl>
 
@@ -67,9 +203,18 @@ const FormCategory = () => {
                                     <Input
                                         type="text"
                                         id="description"
+                                        value={formik.values.description}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
                                     />
+                                    {formik.touched.description && formik.errors.description ? (
+                                        <Alert justifyContent="center" status="error">
+                                            <AlertIcon />
+                                            {formik.errors.description}
+                                        </Alert>
+                                    ) : null}
                                 </FormControl>
-                                {test
+                                {cate
                                     ? <Button
                                         borderRadius={0}
                                         type="submit"
@@ -93,6 +238,7 @@ const FormCategory = () => {
                         </form>
                     </Box>
                 </Stack>
+            }
         </Flex>
     );
 };
--- src/features/auth/formLogin.js
+++ src/features/auth/formLogin.js
@@ -19,12 +19,7 @@ import { login } from '../../services/authService'
 import { useSelector, useDispatch } from 'react-redux';
 import { authLog, selectAuth } from '../../reducers/authReducer';
 
-import React, { useEffect } from 'react';
-import FormLogin from "../features/auth/formLogin"
-import { useSelector } from 'react-redux';
-import { selectAuth } from '../reducers/authReducer';
-import { useToast } from '@chakra-ui/react';
-import AvoidAuthRedundancies from "../features/auth/AvoidAuthRedundancies";
+const FormLogin = () => {
 
     const toast = useToast();
     const auth = useSelector(selectAuth);
@@ -59,15 +54,16 @@ import AvoidAuthRedundancies from "../features/auth/AvoidAuthRedundancies";
 
     //Funcion para llamar alertas de Login
 
-    useEffect(() => {
-        if (log.state === 'success') {
+    const handleFeedback = (alertType) => {
+        if (alertType){
             toast({
-                description: "Bienvenido!",
-                status: "success",
-                duration: 2000,
-                isClosable: true,
-            });
-        } else if (log.state === 'error') {
+				description: "Bienvenido!",
+				status: "success",
+				duration: 2000,
+				isClosable: true,
+			});
+        }
+        else{
             toast({
 				description: "Email o contraseña incorrecta",
 				status: "error",
@@ -75,13 +71,87 @@ import AvoidAuthRedundancies from "../features/auth/AvoidAuthRedundancies";
 				isClosable: true,
 			});
         }
-    });
+    }
 
     return (
-        <AvoidAuthRedundancies>
-			<FormLogin />
-		</AvoidAuthRedundancies>
+        <Flex
+            flexDirection="column"
+            width="100wh"
+            height="100vh"
+            backgroundColor="gray.200"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Stack
+                flexDir="column"
+                mb="2"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Avatar bg="teal.500" />
+                <Heading color="teal.400">Login</Heading>
+                <Box minW={{ base: "90%", md: "468px" }}>
+                    <form
+                        onSubmit={formik.handleSubmit}
+                    >
+                        <Stack
+                            spacing={4}
+                            p="1rem"
+                            backgroundColor="whiteAlpha.900"
+                            boxShadow="md"
+                        >
+                            <FormControl mt={2}>
+                                {formik.touched.email && formik.errors.email ? (
+                                    <Alert justifyContent="center" status="error">
+                                        <AlertIcon />
+                                        {formik.errors.email}
+                                    </Alert>
+                                ) : null}
+                                <FormLabel>Email</FormLabel>
+                                <Input
+                                    type="email"
+                                    placeholder="Mquinola@maquinola.com"
+                                    id="email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                            </FormControl>
+
+                            <FormControl mt={2}>
+                                {formik.touched.password && formik.errors.password ? (
+                                    <Alert justifyContent="center" status="error">
+                                        <AlertIcon />
+                                        {formik.errors.password}
+                                    </Alert>
+                                ) : null}
+                                <FormLabel>Password</FormLabel>
+                                <Input
+                                    type="password"
+                                    placeholder="*******"
+                                    id="password"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+
+                            </FormControl>
+                            <Button
+                                borderRadius={0}
+                                type="submit"
+                                variant="solid"
+                                colorScheme="teal"
+                                width="full"
+                            >
+                                Login
+                            </Button>
+                        </Stack>
+                    </form>
+                </Box>
+            </Stack>
+        </Flex>
+
     );
 }
 
-export default Login;
+export default FormLogin;
