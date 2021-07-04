c1427e609ad3639912235083677e107f88044d57
Elyelin Carrasquero <elyelin15@gmail.com>
2021-07-04 00:56:08 -0300

    validacion con formik para componente form news

--- src/backoffice/news/NewsForm.js
+++ src/backoffice/news/NewsForm.js
@@ -10,10 +10,12 @@ import {
   Stack,
   useToast,
 } from "@chakra-ui/react";
+import { ErrorMessage, Form, Formik } from "formik";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { getCategories } from "../../../services/categories.service";
-import { createNews, updateNews } from "../../../services/newsService";
+import { getCategories } from "../../services/categories.service";
+import { createNews, updateNews } from "../../services/newsService";
+import { newsSchema } from "../../validations/newsSchema";
 
 const defaultNew = {
   name: "",
@@ -24,16 +26,28 @@ const defaultNew = {
 
 export default function NewsForm({ data = defaultNew }) {
   const [categories, setCategories] = useState([]);
-  const [values, setValues] = useState(data);
+  const [image, setImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
   const toast = useToast();
   let history = useHistory();
+
+  console.log({ data });
 
   const submitText = data?.id ? "Guardar" : "Crear";
   const formTitle = data?.id ? "Editar Novedad" : "Crear Novedad";
 
   useEffect(() => {
-    setValues(data);
-  }, [data]);
+    if (data?.image) {
+      if (previewImage === null) {
+        setPreviewImage(data.image);
+      }
+
+      if (image === null) {
+        setImage(data.image);
+      }
+    }
+  }, [data, image, previewImage]);
 
   useEffect(() => {
     getCategories()
@@ -45,27 +59,20 @@ export default function NewsForm({ data = defaultNew }) {
       });
   }, []);
 
-  const handleChangeInput = (e) => {
-    const value = e.target.value;
-    const name = e.target.name;
-
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
+  const onSubmit = (values, { setSubmitting }) => {
     // Actualizar
+    const payload = {
+      ...values,
+      image,
+    };
     if (data?.id) {
-      updateNews(data?.id, values)
+      updateNews(data?.id, payload)
         .then(() => {
           toast({
             title: "Novedad actualizada.",
             status: "success",
           });
+
           history.push("/backoffice/news");
         })
         .catch((e) => {
@@ -73,10 +80,13 @@ export default function NewsForm({ data = defaultNew }) {
             title: "Ocurrio un error al crear la novedad.",
             status: "error",
           });
+        })
+        .finally(() => {
+          setSubmitting(false);
         });
     } else {
       // Crear
-      createNews(values)
+      createNews(payload)
         .then(() => {
           toast({
             title: "Novedad creada.",
@@ -89,11 +99,12 @@ export default function NewsForm({ data = defaultNew }) {
             title: "Ocurrio un error al actualizar la novedad.",
             status: "error",
           });
+        })
+        .finally(() => {
+          setSubmitting(false);
         });
     }
   };
-
-  const [previewImage, setPreviewImage] = useState(null);
 
   const toBase64 = (file) =>
     new Promise((resolve, reject) => {
@@ -103,7 +114,9 @@ export default function NewsForm({ data = defaultNew }) {
       reader.onerror = (error) => reject(error);
     });
 
-  const handleImage = async (e) => {
+  const handleImage = async (e, handleChange) => {
+    handleChange(e);
+
     if (e.target.files.length === 0) {
       setPreviewImage(null);
       return;
@@ -117,124 +130,127 @@ export default function NewsForm({ data = defaultNew }) {
       return;
     }
 
-    setValues({
-      ...values,
-      image: result,
-    });
+    setImage(result);
   };
 
   return (
-    <div>
-      <Flex flexDirection="column" justifyContent="center" alignItems="center">
-        <Stack
-          flexDir="column"
-          mb="2"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Heading color="teal.400" my="6">
-            {formTitle}
-          </Heading>
-          <Box>
-            <form onSubmit={handleSubmit}>
-              <Stack
-                spacing={4}
-                p="1rem"
-                backgroundColor="whiteAlpha.900"
-                boxShadow="md"
-              >
-                <FormControl mt={2}>
-                  <FormLabel>Título</FormLabel>
-                  <Input
-                    name="name"
-                    type="text"
-                    placeholder=""
-                    value={values?.name}
-                    onChange={handleChangeInput}
-                  />
-                </FormControl>
-
-                <FormControl mt={2}>
-                  <FormLabel>Imagen</FormLabel>
-                  <Input
-                    name="image"
-                    type="file"
-                    id="content"
-                    onChange={handleImage}
-                  />
-
-                  {previewImage && (
-                    <div>
-                      <img src={previewImage} alt="" />
-                    </div>
-                  )}
-                </FormControl>
-
-                <FormControl mt={2}>
-                  <FormLabel>Contenido</FormLabel>
-                  <Input
-                    name="content"
-                    type="text"
-                    placeholder=""
-                    value={values?.content}
-                    onChange={handleChangeInput}
-                  />
-                </FormControl>
-
-                <FormControl mt={2}>
-                  <FormLabel>Categoría</FormLabel>
-                  <Select
-                    name="category_id"
-                    placeholder="-- Seleccione --"
-                    onChange={handleChangeInput}
-                  >
-                    {categories.map((c) => {
-                      return (
-                        <option key={c.id} value={c.id}>
-                          {c.name}
-                        </option>
-                      );
-                    })}
-                  </Select>
-                </FormControl>
-                <Button
-                  borderRadius={0}
-                  type="submit"
-                  variant="solid"
-                  colorScheme="teal"
-                  width="full"
+    <Formik
+      initialValues={data}
+      validationSchema={newsSchema}
+      onSubmit={onSubmit}
+      enableReinitialize
+    >
+      {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+        <Form className="form-new-post" onSubmit={handleSubmit} noValidate>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Stack
+              flexDir="column"
+              mb="2"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Heading color="teal.400" my="6">
+                {formTitle}
+              </Heading>
+              <Box>
+                <Stack
+                  spacing={4}
+                  p="1rem"
+                  backgroundColor="whiteAlpha.900"
+                  boxShadow="md"
                 >
-                  {submitText}
-                </Button>
-              </Stack>
-            </form>
-          </Box>
-        </Stack>
-      </Flex>
-    </div>
+                  <FormControl mt={2}>
+                    <FormLabel>Título</FormLabel>
+                    <Input
+                      name="name"
+                      type="text"
+                      autoFocus
+                      placeholder=""
+                      value={values?.name}
+                      onChange={handleChange}
+                    />
+
+                    <Box color="red.500">
+                      <ErrorMessage name="name" component="small" />
+                    </Box>
+                  </FormControl>
+
+                  <FormControl mt={2}>
+                    <FormLabel>Imagen</FormLabel>
+                    <Input
+                      name="image"
+                      type="file"
+                      onChange={(e) => {
+                        handleImage(e, handleChange);
+                      }}
+                    />
+
+                    {previewImage && (
+                      <div>
+                        <img src={previewImage} alt="" />
+                      </div>
+                    )}
+
+                    <Box color="red.500">
+                      <ErrorMessage name="image" component="small" />
+                    </Box>
+                  </FormControl>
+
+                  <FormControl mt={2}>
+                    <FormLabel>Contenido</FormLabel>
+                    <Input
+                      name="content"
+                      type="text"
+                      placeholder=""
+                      value={values?.content}
+                      onChange={handleChange}
+                    />
+                    <Box color="red.500">
+                      <ErrorMessage name="content" component="small" />
+                    </Box>
+                  </FormControl>
+
+                  <FormControl mt={2}>
+                    <FormLabel>Categoría</FormLabel>
+                    <Select
+                      name="category_id"
+                      placeholder="-- Seleccione --"
+                      onChange={handleChange}
+                      value={values?.category_id}
+                    >
+                      {categories.map((c) => {
+                        return (
+                          <option key={c.id} value={c.id}>
+                            {c.name}
+                          </option>
+                        );
+                      })}
+                    </Select>
+
+                    <Box color="red.500">
+                      <ErrorMessage name="category_id" component="small" />
+                    </Box>
+                  </FormControl>
+                  <Button
+                    borderRadius={0}
+                    type="submit"
+                    variant="solid"
+                    colorScheme="teal"
+                    width="full"
+                    disabled={isSubmitting}
+                  >
+                    {submitText}
+                  </Button>
+                </Stack>
+              </Box>
+            </Stack>
+          </Flex>
+        </Form>
+      )}
+    </Formik>
   );
 }
-/*const validation = (values) => {
-  var errors = {};
-  if (!values.name) {
-    errors.name = "Name is required!";
-  } else if (values.name.length <= 1) {
-    errors.name = "Name has to be 1 character at less!";
-  }
-  return errors;
-};
-const Basic = () => (
-  <Formik
-    initialValues={{
-      name: "",
-    }}
-    onSubmit={(values, actions) => {
-      setTimeout(() => {
-        console.log(values);
-        console.log(actions);
-        actions.setSubmitting(false);
-      }, 2000);
-    }}
-    validate={validation}
-  ></Formik>
-);*/
