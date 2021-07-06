fc46ff2b1933c90c1d225fefc1198e75c0f3b3dd
Jose Sierra <segjsierra@gmail.com>
2021-07-05 22:57:51 -0300

    Resolviendo conflicto con pages

--- src/config/RouterManager/Pages.js
+++ src/config/RouterManager/Pages.js
@@ -1,15 +1,14 @@
-import { Example1 } from '../../pages/Example1'
-import { Example2 } from '../../pages/Example2'
-import { ActivitiesPage } from '../../pages/ActivitiesPage'
-import { CategoriesPage } from '../../pages/CategoriesPage'
-import ComponentScreenListOfNews from '../../backoffice/ComponentScreenListOfNews'
-import ComponentCreateNews from '../../backoffice/ComponentCreateNews'
-import Registro from "../../pages/Registro"
-import Login from '../../pages/Login'
-import BackofficePage from '../../pages/Backoffice'
-import FormCategory from '../../backoffice/categories/FormCategory'
+import { Example1 } from "../../pages/Example1";
+import { Example2 } from "../../pages/Example2";
+import { ActivitiesPage } from "../../pages/ActivitiesPage";
+import { CategoriesPage } from "../../pages/CategoriesPage";
+import ComponentScreenListOfNews from "../../backoffice/ComponentScreenListOfNews";
+import ComponentCreateNews from "../../backoffice/ComponentCreateNews";
+import Registro from "../../pages/Registro";
+import Login from "../../pages/Login";
+import BackofficePage from "../../pages/Backoffice";
+import FormCategory from "../../backoffice/categories/FormCategory";
 import ActivitiesCreate from "../../pages/ActivitiesCreate"
-
 
 /*
   component = Ahi iria la pagina
@@ -19,26 +18,42 @@ import ActivitiesCreate from "../../pages/ActivitiesCreate"
 */
 
 class Page {
-  constructor(component, path, exact=true){
-    this.component = component
-    this.path = path
-    this.key = path
-    this.exact = exact
+  constructor(component, path, exact = true) {
+    this.component = component;
+    this.path = path;
+    this.key = path;
+    this.exact = exact;
   }
 }
 
 export const Pages = [
-  new Page(Example1, '/'),
-  new Page(Example2, '/example2'),
-  new Page(BackofficePage, '/backoffice'),
-  new Page(ComponentScreenListOfNews, '/backoffice/news'),
-  new Page(ComponentCreateNews, '/backoffice/news/create'),
-  new Page(ActivitiesPage, '/backoffice/activities'),
+  new Page(Example1, "/"),
+  new Page(Example2, "/example2"),
+  new Page(BackofficePage, "/backoffice"),
+  new Page(ComponentScreenListOfNews, "/backoffice/news"),
+  new Page(ComponentCreateNews, "/backoffice/news/create"),
+  new Page(ActivitiesPage, "/backoffice/activities"),
   new Page(ActivitiesCreate, '/backoffice/activities/create'),
   new Page(ActivitiesCreate, '/backoffice/activities/:id'),
-  new Page(CategoriesPage, '/backoffice/categories'),
-  new Page(Registro, '/Registro'),
-  new Page(Login, '/Login'),
-  new Page (FormCategory, '/backoffice/categories/create'),
-  new Page (FormCategory, '/backoffice/categories/:id')
-]
+  new Page(CategoriesPage, "/backoffice/categories"),
+  new Page(Registro, "/Registro"),
+  new Page(Login, "/Login"),
+  new Page(FormCategory, "/backoffice/categories/create"),
+  new Page(FormCategory, "/backoffice/categories/:id"),
+ 
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
