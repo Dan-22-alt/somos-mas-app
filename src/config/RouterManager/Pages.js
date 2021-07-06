d602aaeddf6be15f2ab3bd8ad7e11607222ff979
Francoveliz <francoleandroveliz@gmail.com>
2021-07-06 01:48:24 -0300

    added list testimonials route

--- src/config/RouterManager/Pages.js
+++ src/config/RouterManager/Pages.js
@@ -11,7 +11,7 @@ import FormCategory from "../../backoffice/categories/FormCategory";
 import EditNewsPage from "../../pages/backoffice/news/EditNewsPage";
 import EditSlidePage from "../../pages/backoffice/slide/EditSlidePage";
 import CreateSlidePage from "../../pages/backoffice/slide/CreateSlidePage";
-
+import ListTestimonials from "../../backoffice/testimonials/list/ListTestimonials";
 /*
   component = Ahi iria la pagina
   path = endpoind de la url
@@ -20,27 +20,28 @@ import CreateSlidePage from "../../pages/backoffice/slide/CreateSlidePage";
 */
 
 class Page {
-  constructor(component, path, exact = true) {
-    this.component = component;
-    this.path = path;
-    this.key = path;
-    this.exact = exact;
-  }
+	constructor(component, path, exact = true) {
+		this.component = component;
+		this.path = path;
+		this.key = path;
+		this.exact = exact;
+	}
 }
 
 export const Pages = [
-  new Page(Example1, "/"),
-  new Page(Example2, "/example2"),
-  new Page(BackofficePage, "/backoffice"),
-  new Page(ComponentScreenListOfNews, "/backoffice/news"),
-  new Page(ComponentCreateNews, "/backoffice/news/create"),
-  new Page(ActivitiesPage, "/backoffice/activities"),
-  new Page(CategoriesPage, "/backoffice/categories"),
-  new Page(Registro, "/Registro"),
-  new Page(Login, "/Login"),
-  new Page(FormCategory, "/backoffice/categories/create"),
-  new Page(FormCategory, "/backoffice/categories/:id"),
-  new Page(EditNewsPage, "/backoffice/news/:id/edit"),
-  new Page(CreateSlidePage, "/backoffice/slides/create"),
-  new Page(EditSlidePage, "/backoffice/slides/:id"),
+	new Page(Example1, "/"),
+	new Page(Example2, "/example2"),
+	new Page(BackofficePage, "/backoffice"),
+	new Page(ComponentScreenListOfNews, "/backoffice/news"),
+	new Page(ComponentCreateNews, "/backoffice/news/create"),
+	new Page(ActivitiesPage, "/backoffice/activities"),
+	new Page(CategoriesPage, "/backoffice/categories"),
+	new Page(Registro, "/Registro"),
+	new Page(Login, "/Login"),
+	new Page(FormCategory, "/backoffice/categories/create"),
+	new Page(FormCategory, "/backoffice/categories/:id"),
+	new Page(EditNewsPage, "/backoffice/news/:id/edit"),
+	new Page(CreateSlidePage, "/backoffice/slides/create"),
+	new Page(EditSlidePage, "/backoffice/slides/:id"),
+	new Page(ListTestimonials, "/backoffice/testimonials"),
 ];
