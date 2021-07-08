07f88a3a39cc11d425e9da0ae1424b6a45176923
Elyelin Carrasquero <elyelin15@gmail.com>
2021-07-08 12:50:59 -0300

    toast

--- src/backoffice/activities/ListOfActivities.js
+++ src/backoffice/activities/ListOfActivities.js
@@ -1,40 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { SimpleGrid } from "@chakra-ui/react"
-import { Activity } from './Activity'
+import React, { useEffect, useState } from "react";
+import { SimpleGrid, useToast } from "@chakra-ui/react";
+import { Activity } from "./Activity";
+import { useHistory } from "react-router-dom";
+import { createActivity, deleteActivity } from "../../services/activityService";
 
 export const ListOfActivities = () => {
   const [activities, setActivities] = useState({
-    loading: true, res: [], error: ''
-  })
+    loading: true,
+    res: [],
+    error: "",
+  });
+  const [deleteAlertIsOpen, setDeleteAlertIsOpen] = useState(false);
+  const toast = useToast();
+  let history = useHistory();
 
-  const deleteItem = id => setActivities(prevState =>
-    ({...prevState, res: prevState.res.filter(item => item.id !== id)}))
+  const deleteItem = (id) => {
+    setDeleteAlertIsOpen(true);
+    deleteActivity(id)
+      .then(() => {
+        toast({
+          title: "Actividad eliminada.",
+          status: "success",
+        });
+        window.location.reload();
+        history.push("/backoffice/activities");
+      })
+      .catch((e) => {
+        toast({
+          title: "Ocurrio un error al eliminar la actividad.",
+          status: "error",
+        });
+      });
+  };
 
   useEffect(() => {
-    fetch('http://ongapi.alkemy.org/api/activities')
-      .then(r => r.json())
-      .then(r => setActivities(prevState =>
-        ({...prevState, res: r.data, loading: false})))
-      .catch( error => setActivities(prevState =>
-        ({...prevState, error, loading: false})))
-  }, [])
+    fetch("http://ongapi.alkemy.org/api/activities")
+      .then((r) => r.json())
+      .then((r) =>
+        setActivities((prevState) => ({
+          ...prevState,
+          res: r.data,
+          loading: false,
+        }))
+      )
+      .catch((error) =>
+        setActivities((prevState) => ({ ...prevState, error, loading: false }))
+      );
+  }, []);
 
-  return(
+  return (
     <SimpleGrid
-      my='50px'
+      my="50px"
       minChildWidth="350px"
-      mt='150px'
-      justifyItems='center'
+      mt="150px"
+      justifyItems="center"
       spacing="40px"
       mx={[0, 5, 10, 30]}
     >
-      {activities?.res.map( activity =>
+      {activities?.res.map((activity) => (
         <Activity
-          key={'activity ' + activity.id}
+          key={"activity " + activity.id}
           deleteItem={deleteItem}
           {...activity}
         />
-      )}
+      ))}
     </SimpleGrid>
-  )
-}
+  );
+};
