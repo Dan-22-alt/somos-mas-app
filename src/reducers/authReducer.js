774c9380ec7ae58c0302adfca956d6617543b8d8
Francoveliz <francoleandroveliz@gmail.com>
2021-07-14 03:43:17 -0300

    hotfix the lost changes were recovered

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,47 +1,71 @@
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios'
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 // AUTH STATE
 const authState = {
-  token: '',
-  error: '',
-  state: null
+	token: "",
+	error: "",
+	user: null,
+	loading: false,
+	state: null,
 };
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: authState,
-  reducers: {
-    loginSuccess: (state, action) => {
-      state.token = action.payload.token;
-      state.user = action.payload.user;
-      state.state = 'success'
-    },
-    loginFailed: (state, action) => {
-      state.error = action.payload;
-      state.state = 'error'
+	name: "auth",
+	initialState: authState,
+	reducers: {
+		loginSuccess: (state, action) => {
+			state.token = action.payload.token;
+			state.user = action.payload.user;
+			state.token = action.payload.token;
+			localStorage.setItem("token", action.payload.token);
+			state.user = action.payload.user;
+			state.state = "success";
+		},
+		loginFailed: (state, action) => {
+			state.error = action.payload;
+			state.state = "error";
+		},
+		signInLoading: state => {
+			state.loading = true;
+		},
 
-    },
-  },
+		signInSuccess: (state, action) => {
+			state.token = action.payload.token;
+			localStorage.setItem("token", action.payload.token);
+			state.loading = false;
+		},
+		signInFail: (state, action) => {
+			state.error = action.payload.error;
+			state.loading = false;
+		},
+	},
 });
 
-export const { loginSuccess, loginFailed } = authSlice.actions;
+export const {
+	loginSuccess,
+	loginFailed,
+	signInLoading,
+	signInSuccess,
+	signInFail,
+} = authSlice.actions;
 
 export const selectAuth = state => state.auth;
 
 export const authLog = values => dispatch => {
-  axios.post(`${process.env.REACT_APP_API_LOGIN}`, values)
-    .then(res => {
-      if (res.data.error){
-        dispatch(loginFailed(res.data.error));
-      } else if (res.data.data.token){
-        dispatch(loginSuccess(res.data.data));
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      return true
-    })
+	axios
+		.post(`${process.env.REACT_APP_API_LOGIN}`, values)
+		.then(res => {
+			if (res.data.error) {
+				dispatch(loginFailed(res.data.error));
+			} else if (res.data.data.token) {
+				dispatch(loginSuccess(res.data.data));
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			return true;
+		});
 };
 
 export default authSlice.reducer;
