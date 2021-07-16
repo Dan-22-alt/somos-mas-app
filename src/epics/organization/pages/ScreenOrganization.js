bfc91eb206421c9542a1187ae5ff8304fc41d965
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-16 12:04:13 -0300

    ScreenOrganization file created

---
+++ src/epics/organization/pages/ScreenOrganization.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScreenOrganization = () => {
+    return (
+        <div>
+            <h1>DATOS DE LA ORGANIZACION</h1>
+        </div>
+    )
+}
+
+export default ScreenOrganization
