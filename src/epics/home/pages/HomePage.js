43bdc0414d7e2744a49d1dcc7f16976ef158a35e
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-16 00:02:43 -0300

    AsyncThunk for login reducer created

--- src/epics/home/pages/HomePage.js
+++ src/epics/home/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { HomeEditForm } from "../components/HomeEditForm";
+import LogOutButt from "../../logout/LogOutButt"
 
 export const HomePage = () => {
 	const admin = true;
-	return <>{admin && <HomeEditForm />}</>;
+	return <>{admin && <HomeEditForm />}<LogOutButt /></>;
 };
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { register } from "../services/authService";
+import { register, login } from "../services/authService";
 
 // ! SIGN UP FUNCTIONALITY
 
@@ -39,6 +38,25 @@ USAGE:{
 }
 */
 
+// SIGN IN ASYNCTUNK
+
+export const signInUser = createAsyncThunk(
+	"auth/signInUser",
+	async (values, { rejectWithValue }) => {
+		try {
+			const response = await login(values);
+			if (response.data.error) {
+				return rejectWithValue(response.data);
+			} else {
+				return response.data.data;
+			}
+		} catch (error) {
+			console.log("Error", error.response.data);
+			return rejectWithValue(error.response.data);
+		}
+	}
+);
+
 // AUTH STATE
 const authState = {
 	token: "",
@@ -52,33 +70,37 @@ const authSlice = createSlice({
 	name: "auth",
 	initialState: authState,
 	reducers: {
-		loginSuccess: (state, action) => {
-			state.token = action.payload.token;
-			state.user = action.payload.user;
-			state.token = action.payload.token;
-			localStorage.setItem("token", action.payload.token);
-			state.user = action.payload.user;
-			state.state = "success";
+		logoutSuccess: (state, action) => {
+			state.token = ''
+			state.user = {}
+			localStorage.removeItem("token")
+			state.state = "success"
 		},
-		loginFailed: (state, action) => {
-			state.error = action.payload;
-			state.state = "error";
-		},
-    logoutSuccess: (state, action) => {
-      state.token = ''
-      state.user = {}
-      localStorage.removeItem("token")
-      state.state = "success"
-    }
 	},
 	extraReducers: {
+		//signInUser
+		[signInUser.pending]: state => {
+			state.loading = true
+		},
+		[signInUser.fulfilled]: (state, { payload }) => {
+			localStorage.setItem("token", payload.token);
+			state.token = payload.token;
+			state.user = payload.user;
+			state.loading = false;
+			state.state = "success";
+		},
+		[signInUser.rejected]: (state, { payload }) => {
+			state.error = payload.message;
+			state.state = "error";
+			state.loading = false;
+		},
 		//signUpUser
 		[signUpUser.pending]: state => {
 			state.loading = true;
 		},
 		[signUpUser.rejected]: (state, { payload }) => {
 			state.state = "error";
-			state.error = payload.message;
+			state.error = payload.message; 
 			state.loading = false;
 		},
 		[signUpUser.fulfilled]: (state, { payload }) => {
@@ -90,24 +112,8 @@ const authSlice = createSlice({
 	},
 });
 
-export const { loginSuccess, loginFailed, logoutSuccess } = authSlice.actions;
+export const { logoutSuccess } = authSlice.actions;
 
 export const selectAuth = state => state.auth;
-
-export const authLog = values => dispatch => {
-	axios
-		.post(`${process.env.REACT_APP_API_LOGIN}`, values)
-		.then(res => {
-			if (res.data.error) {
-				dispatch(loginFailed(res.data.error));
-			} else if (res.data.data.token) {
-				dispatch(loginSuccess(res.data.data));
-			}
-		})
-		.catch(err => {
-			console.log(err);
-			return true;
-		});
-};
 
 export default authSlice.reducer;
