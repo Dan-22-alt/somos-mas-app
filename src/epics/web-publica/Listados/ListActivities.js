aa24c6d7ea4a50e7582cff3b75cba6ef42b346e9
aibbya <aibbyale@gmail.com>
2021-07-15 23:22:07 -0300

    componentes para listar Novedades y Actividades, trae la data desde el reducer, y rederiza con el componente card

---
+++ src/epics/web-publica/Listados/ListActivities.js
@@ -0,0 +1,47 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import { Flex, Container, SimpleGrid, Stack, Heading, Center, Text } from "@chakra-ui/react";
+import { Card } from './Components/Card';
+import { useDispatch, useSelector } from "react-redux"
+import { fetchActivities } from "../../../reducers/activitiesSlice"
+
+
+const ListActivities = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchActivities())
+    }, []);
+    const data = useSelector(state => state.activities);
+    const actividades = Object.values(data.entities)
+    console.log(actividades);
+
+    return (
+        <Fragment>
+            <Flex>
+                <Heading align="center" mx={"auto"} my={5} as="h1" size="2xl">
+                    <Text>
+                        Listado de Actividades
+                    </Text>
+                </Heading>
+            </Flex>
+            <SimpleGrid
+                // my="50px"
+                // minChildWidth="350px"
+                // mt="150px"
+                justifyItems="center"
+                // mx={[0, 5, 10, 30]}
+                columns={["1", "2", "3"]} spacing={5}
+            >
+                {actividades.map(activity => (
+                    <Card
+                        key={"activity " + activity.id}
+                        {...activity}
+                    />
+                ))}
+            </SimpleGrid>
+        </Fragment>
+    )
+}
+
+export default ListActivities
+// {name, id, image, description, handleEdit, handleDelete
