6f35060ffe70b877be996316fde50e38fab204d7
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-19 19:14:46 -0300

    Formik configuration created

--- src/epics/categories/pages/ContactForm.js
+++ src/epics/categories/pages/ContactForm.js
@@ -13,8 +13,47 @@ import {
     Textarea,
     useToast,
 } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const ContactForm = () => {
+
+    const [send, setSend] = useState(false)
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            phone: "",
+            message: "",
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required("Nombre obligatorio")
+                .min(4, "El nombre es muy corto"),
+            email: Yup.string()
+                .email("Email no valido")
+                .required("Email obligatorio"),
+            phone: Yup.string()
+                .required("Telefono obligatorio")
+                .min(8, "El numero es muy corto"),
+            message: Yup.string()
+                .required("Mensaje obligatorio")
+                .min(8, "El mensaje es muy corto"),
+        }),
+        onSubmit: valores => {
+            const values = {
+                name: valores.name,
+                email: valores.email,
+                phone: valores.phone,
+                message: valores.message,
+            };
+            //fUNCIONES PARA EVITAR SPAM DE MENSAJES A REVISAR
+            localStorage.setItem('msjSend', values)
+            setSend(true)
+            //fUNCIONES PARA EVITAR SPAM DE MENSAJES A REVISAR
+        },
+    });
 
     return (
         <Flex
@@ -30,7 +69,7 @@ const ContactForm = () => {
         >
             <Heading color="teal.400">Formulario de contacto</Heading>
             <Box minW={{ base: "90%", md: "468px" }}>
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                     <Stack
                         spacing={4}
                         p="1rem"
@@ -41,7 +80,16 @@ const ContactForm = () => {
                                 type="text"
                                 placeholder="Nombre"
                                 id="name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.name && formik.errors.name && !localStorage.getItem('msjSend') ? (
+                                <Alert justifyContent="center" status="error" marginTop="2vh">
+                                    <AlertIcon />
+                                    {formik.errors.name}
+                                </Alert>
+                            ) : null}
                         </FormControl>
 
                         <FormControl mt={2}>
@@ -50,7 +98,16 @@ const ContactForm = () => {
                                 type="email"
                                 placeholder="ejemplo@gmail.com"
                                 id="email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.email && formik.errors.email && !localStorage.getItem('msjSend') ? (
+                                <Alert justifyContent="center" status="error" marginTop="2vh">
+                                    <AlertIcon />
+                                    {formik.errors.email}
+                                </Alert>
+                            ) : null}
                         </FormControl>
                         <FormControl mt={2}>
                             <FormLabel>Telefono</FormLabel>
@@ -58,7 +115,16 @@ const ContactForm = () => {
                                 type="number"
                                 placeholder="1122334455"
                                 id="phone"
+                                value={formik.values.phone}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.phone && formik.errors.phone && !localStorage.getItem('msjSend') ? (
+                                <Alert justifyContent="center" status="error" marginTop="2vh">
+                                    <AlertIcon />
+                                    {formik.errors.phone}
+                                </Alert>
+                            ) : null}
                         </FormControl>
                         <FormControl mt={2}>
                             <FormLabel>Mensaje</FormLabel>
@@ -66,9 +132,18 @@ const ContactForm = () => {
                                 type="text"
                                 placeholder=""
                                 id="message"
+                                value={formik.values.message}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.message && formik.errors.message && !localStorage.getItem('msjSend') ? (
+                                <Alert justifyContent="center" status="error" marginTop="2vh">
+                                    <AlertIcon />
+                                    {formik.errors.message}
+                                </Alert>
+                            ) : null}
                         </FormControl>
-                        {localStorage.getItem('msjSend') 
+                        {localStorage.getItem('msjSend') || send
                             ? <Box
                                 borderRadius={0}
                                 type="submit"
