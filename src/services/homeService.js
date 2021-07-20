646e3dc9d6125f1fedd5dee59e2d281a588a60d7
Francoveliz <francoleandroveliz@gmail.com>
2021-07-20 18:51:48 -0300

    added homeService.js

---
+++ src/services/homeService.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export const fetchOrganizationData = async () => {
+	try {
+		const response = await axios.get(
+			process.env.REACT_APP_API_ORGANIZATION
+		);
+		const data = await response.data.data[0];
+		return data;
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+};
+
+export const setOrganizationData = async ({ ...body }) => {
+	try {
+		const response = await axios.post(
+			process.env.REACT_APP_API_ORGANIZATION,
+			{ ...body }
+		);
+		const { data, success, message } = await response.data;
+		return { data, success, message };
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+};
+
+/*
+USAGE:{
+  const body = {
+    name: "new name",
+    address: "new address"
+  }
+
+  setOrganizationData(body)
+}
+*/
