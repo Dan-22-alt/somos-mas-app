4117adf77c424efbdfa61a9def60c56a3c8303db
aibbya <aibbyale@gmail.com>
2021-07-20 20:56:00 -0300

    codigo para consumir lista de miembros

--- src/epics/nosotros/Index.js
+++ src/epics/nosotros/Index.js
@@ -2,41 +2,59 @@ import React, { useEffect, useState } from "react";
 import { Box, Container, Heading, Stack } from "@chakra-ui/react";
 import { getData } from "../../services/organizationService";
 import Description from "./components/Description";
-import MemberList from "./../members/components/MemberList";
 import Title from "../../components/Title";
+import { getAll } from "../../services/membersService";
+import ComponentSkeleton from './../../layout/ComponentSkeleton';
+import NosotrosMemberList from './components/NosotrosMemberList';
 
 const Index = () => {
-  const getDatos = getData();
-  const [data, setData] = useState({});
+    const getDatos = getData();
+    const [data, setData] = useState({});
+    //   const getMembers = getAll()
+    const [members, setMembers] = useState()
 
-  useEffect(() => {
-    if (getDatos.res?.data) {
-      setData(getDatos.res.data[0]);
+    useEffect(() => {
+        if (getDatos.res?.data) {
+            setData(getDatos.res.data[0]);
+        }
+
+    }, [getDatos]);
+
+    async function getMem() {
+         await getAll().then(res => {
+            setMembers(res.data)
+        })
     }
-  }, [getDatos]);
-  console.log(data);
 
-  return (
-    <Container maxW={"3xl"}>
-      <Stack
-        as={Box}
-        textAlign={"center"}
-        spacing={{ base: 8, md: 14 }}
-        py={{ base: 20, md: 36 }}
-      >
-        {/*COMPONENTE DE TITULO NOSOTROS*/}
-        <Title title="Nosotros"></Title>
-        <Heading align="center" mx={"auto"} my={5} as="h1" size="2xl">
-          Sobre nosotros
-        </Heading>
-        <Description text={data.short_description}>{}</Description>
-        <Heading align="center" mx={"auto"} mb={0} as="h1" size="2xl">
-          Miembros
-        </Heading>
-        {/* ACA VA EL COMPONENTE DE DE LISTAR MIEMBROS- TICKET 104" ------------------------------------------------ */}
-      </Stack>
-    </Container>
-  );
+    if (!members) {
+        getMem()
+    }
+
+    return (
+        <Container maxW={"3xl"}>
+            <Stack
+                as={Box}
+                textAlign={"center"}
+                spacing={{ base: 8, md: 14 }}
+                py={{ base: 20, md: 36 }}
+            >
+                <Title title="Nosotros"></Title>
+                <Heading align="center" mx={"auto"} my={5} as="h1" size="2xl">
+                    Sobre nosotros
+                </Heading>
+                <Description text={data.short_description}>{ }</Description>
+                <Heading align="center" mx={"auto"} mb={0} as="h1" size="2xl">
+                    Miembros
+                </Heading>
+                {
+                    members ?
+                        (<NosotrosMemberList members={members} ></NosotrosMemberList>)
+                        :
+                        (<ComponentSkeleton></ComponentSkeleton>)
+                }
+            </Stack>
+        </Container>
+    );
 };
 
 export default Index;
