ebe7435b1d2814d0c918f6eadc4e4619ce0e8a6a
Elyelin Carrasquero <elyelin15@gmail.com>
2021-07-20 13:03:21 -0300

    Agrego componente Titulo a pagina 'nosotros'

--- src/epics/nosotros/Index.js
+++ src/epics/nosotros/Index.js
@@ -1,48 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import {
-    Box,
-    Container,
-    Heading,
-    Stack,
-} from '@chakra-ui/react';
-import { getData } from '../../services/organizationService';
-import Description from './components/Description';
-import MemberList from './../members/components/MemberList';
-
+import React, { useEffect, useState } from "react";
+import { Box, Container, Heading, Stack } from "@chakra-ui/react";
+import { getData } from "../../services/organizationService";
+import Description from "./components/Description";
+import MemberList from "./../members/components/MemberList";
+import Title from "../../components/Title";
 
 const Index = () => {
-    const getDatos = getData();
-    const [data, setData] = useState({});
+  const getDatos = getData();
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        if (getDatos.res?.data) {
-            setData(getDatos.res.data[0]);
+  useEffect(() => {
+    if (getDatos.res?.data) {
+      setData(getDatos.res.data[0]);
+    }
+  }, [getDatos]);
+  console.log(data);
 
-        }
-    }, [getDatos]);
-    console.log(data);
+  return (
+    <Container maxW={"3xl"}>
+      <Stack
+        as={Box}
+        textAlign={"center"}
+        spacing={{ base: 8, md: 14 }}
+        py={{ base: 20, md: 36 }}
+      >
+        {/*COMPONENTE DE TITULO NOSOTROS*/}
+        <Title title="Nosotros"></Title>
+        <Heading align="center" mx={"auto"} my={5} as="h1" size="2xl">
+          Sobre nosotros
+        </Heading>
+        <Description text={data.short_description}>{}</Description>
+        <Heading align="center" mx={"auto"} mb={0} as="h1" size="2xl">
+          Miembros
+        </Heading>
+        {/* ACA VA EL COMPONENTE DE DE LISTAR MIEMBROS- TICKET 104" ------------------------------------------------ */}
+      </Stack>
+    </Container>
+  );
+};
 
-    return (
-        <Container maxW={'3xl'}>
-            <Stack
-                as={Box}
-                textAlign={'center'}
-                spacing={{ base: 8, md: 14 }}
-                py={{ base: 20, md: 36 }}>
-                {/* ACA VA EL COMPONENTE DE TITULO NOSOTROS-  TICKET 97" ------------------------------------------------ */}
-                <Heading align="center" mx={"auto"} my={5} as="h1" size="2xl">
-                    Sobre nosotros
-                </Heading>
-                <Description text={data.short_description}>{ }</Description>                
-                <Heading align="center" mx={"auto"} mb={0} as="h1" size="2xl">
-                    Miembros
-                </Heading>
-                {/* ACA VA EL COMPONENTE DE DE LISTAR MIEMBROS- TICKET 104" ------------------------------------------------ */}
-                
-            </Stack>
-        </Container>
-
-    )
-}
-
-export default Index
+export default Index;
