db32bf6efbe8f542837befce886cf555f7fd14b6
Francoveliz <francoleandroveliz@gmail.com>
2021-07-20 18:52:35 -0300

    used homeService to provide homePage title

--- src/epics/home/pages/HomePage.js
+++ src/epics/home/pages/HomePage.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HomeEditForm } from "../components/HomeEditForm";
 import { Welcome } from "../components/Welcome";
 import { MainSlide } from "../components/HomeTemplates/MainSlide";
 import { Section } from "../components/HomeTemplates/Section";
-import LogOutButt from "../../logout/LogOutButt"
+import LogOutButt from "../../logout/LogOutButt";
+import { fetchOrganizationData } from "../../../services/homeService";
 
 export const HomePage = () => {
+	const [homeData, setHomeData] = useState(null);
+
+	useEffect(() => {
+		fetchOrganizationData().then(data => setHomeData(data));
+	}, []);
+
 	const admin = true;
-	return(
-    <>
-      {admin && <HomeEditForm />}
-      <LogOutButt />
-      <MainSlide />
-      <Welcome text='Texto de Bienvenida' />
-      <Section title='Últimas Novedades'/>
-      <Section title='Testimonios'/>
-    </>
-  )
-}
+	return (
+		<>
+			{admin && <HomeEditForm />}
+			<LogOutButt />
+			<MainSlide />
+			<Welcome text={homeData?.welcome_text} />
+			<Section title="Últimas Novedades" />
+			<Section title="Testimonios" />
+		</>
+	);
+};
