961f733708819a37d26f1a187dd946d6a80e4641
aibbya <aibbyale@gmail.com>
2021-07-20 21:51:05 -0300

    identacion

--- src/epics/news/Detail/Index.js
+++ src/epics/news/Detail/Index.js
@@ -5,17 +5,17 @@ import Title from '../../../components/Title';
 const Index = () => {
     return (
         <Container maxW={"3xl"}>
-        <Stack
-          as={Box}
-          textAlign={"center"}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
-        >
-          {/*COMPONENTE DE TITULO NOSOTROS*/}
-          <Title title="DETALLE DE NOVEDAD" ></Title>         
-          
-        </Stack>
-      </Container>
+            <Stack
+                as={Box}
+                textAlign={"center"}
+                spacing={{ base: 8, md: 14 }}
+                py={{ base: 20, md: 36 }}
+            >
+                {/*COMPONENTE DE TITULO NOSOTROS*/}
+                <Title title="DETALLE DE NOVEDAD" ></Title>
+
+            </Stack>
+        </Container>
     )
 }
 
