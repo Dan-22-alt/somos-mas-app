251bb6e753436d5454fb83e16503ae458a936ab4
aibbya <aibbyale@gmail.com>
2021-07-20 21:49:08 -0300

    estructura del componente del titulo

--- src/epics/news/Detail/Index.js
+++ src/epics/news/Detail/Index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Box, Container, Heading, Stack } from "@chakra-ui/react";
+import Title from '../../../components/Title';
 
 const Index = () => {
     return (
-        <div>
-            Detalle de novedades
-        </div>
+        <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          {/*COMPONENTE DE TITULO NOSOTROS*/}
+          <Title title="DETALLE DE NOVEDAD" ></Title>         
+          
+        </Stack>
+      </Container>
     )
 }
 
