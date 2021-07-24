25b0688b9e2dec2312509096ff06ff1fc44310f8
Jose Sierra <segjsierra@gmail.com>
2021-07-24 17:22:08 -0300

    Componente para renderizar a donaciones

---
+++ src/epics/donaciones/components/IndexDonacion.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => {
+    return ( 
+        <h1>Prueba</h1>
+     );
+}
+ 
+export default Index;
---
+++ src/epics/donaciones/pages/DonacionPages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Index from '../components/IndexDonacion';
+
+const Donacion = () => {
+    return ( 
+        <Index/>
+     );
+}
+ 
+export default Donacion;
