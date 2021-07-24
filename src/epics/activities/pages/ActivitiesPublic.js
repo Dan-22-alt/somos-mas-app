957b77f5a18c780397b6eccee6ab013c01ef2942
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-23 21:26:15 -0300

    Title added to activities page

--- src/epics/activities/pages/ActivitiesPublic.js
+++ src/epics/activities/pages/ActivitiesPublic.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Title from "../../../components/Title";
+import { Box, Container, Heading, Stack } from "@chakra-ui/react";
 
 const ActivitiesPublic = () => {
+
+    const image = "https://www.esan.edu.pe/apuntes-empresariales/2016/06/14/ongempresas_principal.jpg"
+
     return (
-        <div>
-            Actividades
-        </div>
+        <Container maxW="3xl">
+            <Stack
+                as={Box}
+                textAlign="center"
+                spacing={{ base: 8, md: 14 }}
+                py={{ base: 20, md: 36 }}
+            >
+                <Title title="Actividades" image={image}/>
+            </Stack>
+        </Container>
     )
 }
 
