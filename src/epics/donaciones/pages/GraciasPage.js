9e24b108406a3979538417f209bd448c952cea0b
Jose Sierra <segjsierra@gmail.com>
2021-07-25 15:49:10 -0300

    Se Agregó la ruta /gracias en pages public

---
+++ src/epics/donaciones/pages/GraciasPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Gracias from '../components/IndexGracias';
+
+const PageGracias = () => {
+    return ( 
+        <Gracias/>
+     );
+}
+ 
+export default PageGracias;
