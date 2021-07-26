68b5d0cb12c793262ee8a176a34c75a86c989895
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-26 11:09:34 -0300

    List and card components created for testimonials

---
+++ src/epics/testimonials/testimonialsPublic/components/CardTestimonialsPublic.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardTestimonialsPublic = () => {
+    return (
+        <div>
+            Cards
+        </div>
+    )
+}
+
+export default CardTestimonialsPublic
---
+++ src/epics/testimonials/testimonialsPublic/components/ListTestimonialsPublic.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListTestimonialsPublic = () => {
+    return (
+        <div>
+            Lista de testimonios
+        </div>
+    )
+}
+
+export default ListTestimonialsPublic
