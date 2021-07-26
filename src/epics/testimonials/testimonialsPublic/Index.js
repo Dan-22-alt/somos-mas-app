5403fb89bd1d0dd82b9f68fb1529562cb402407f
Daniel Alejandro Cisneros <dan.alejandro2210@gmail.com>
2021-07-25 22:11:25 -0300

    Testimonials render component created

--- src/epics/testimonials/testimonialsPublic/Index.js
+++ src/epics/testimonials/testimonialsPublic/Index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Title from "../../../components/Title";
+import { Box, Container, Stack } from "@chakra-ui/react";
 
 const Index = () => {
+
+    const image = "http://www.tusclicks.cl/blog/wp-content/uploads/2013/10/testimonios.png"
+
     return (
-        <div>
-            Testimonios
-        </div>
+        <Container maxW="3xl">
+            <Stack
+                as={Box}
+                textAlign="center"
+                spacing={{ base: 8, md: 14 }}
+                py={{ base: 20, md: 36 }}
+            >
+                <Title title="Testimonios" image={image}/>
+            </Stack>
+        </Container>
     )
 }
 
