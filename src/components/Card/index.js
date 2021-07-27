0696ff609c10a519ca7271af13c5da32ec55feaf
Francoveliz <francoleandroveliz@gmail.com>
2021-07-27 18:31:42 -0300

    fixed spacing

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,23 +5,14 @@ import { ButtonsCard } from './ButtonsCard';
 
 export const Card = ({ name, id, image, description, handleEdit, handleDelete, children, activity, handleViews }) => {
   return (
-    <Box borderWidth="1px" borderRadius="lg" w={[350]} pb="10px" boxShadow="lg" p="1rem">
+    <Box borderWidth="1px" borderRadius="lg" boxShadow="lg" p="1rem">
       <SmartPicture src={image} />
-      <Box>
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          fontSize="20px"
-          as="h4"
-          textAlign="center"
-          my="8px"
-          lineHeight="tight"
-          isTruncated
-        >
+      <Box pt={4}>
+        <Box fontWeight="semibold" fontSize="20px" as="h4" textAlign="center" mb={4} lineHeight="tight" isTruncated>
           {name}
         </Box>
         {description && (
-          <Box mt="1" fontSize="16px" as="p" textAlign="center" my="8px" lineHeight="tight" isTruncated>
+          <Box fontSize="16px" as="p" textAlign="center" mb={7} lineHeight="tight" isTruncated>
             {description}
           </Box>
         )}
