90719d29fc7eb3b095ae4bcc7951260ebdf1b9a6
Jose Sierra <segjsierra@gmail.com>
2021-07-28 13:11:44 -0300

    Card backoffice add

--- src/epics/backoffice/components/Dasboard.js
+++ src/epics/backoffice/components/Dasboard.js
@@ -1,21 +1,41 @@
-import { Box, Flex, Heading, Stack } from '@chakra-ui/react';
+import { Box, Avatar, Button, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 
-const Dasboard = () => {
+const Dasboard = ({name, image}) => {
   return (
-    <Flex
-      flexDirection="column"
-      width="100wh"
-      height="90vh"
-      backgroundColor="gray.200"
-      justifyContent="center"
-      alignItems="center"
+    <Box borderWidth="1px" borderRadius="lg" w={[350]} pb="10px" boxShadow="lg" p="1rem" m="2" 
+    _hover={{
+      bg: 'gray.200',
+    }}
+    _focus={{
+      bg: 'green.500',
+    }}
     >
-      <Stack flexDir="column" mb="2" justifyContent="center" alignItems="center">
-        <Heading color="teal.400">Bienvenido!!!</Heading>
-        <Box minW={{ base: '90%', md: '468px' }}></Box>
-      </Stack>
-    </Flex>
+    <Box>
+      <Box
+        mt="1"
+        fontWeight="semibold"
+        fontSize="20px"
+        color="primary.200"
+        as="h4"
+        textAlign="center"
+        my="8px"
+        lineHeight="tight"
+        isTruncateds
+      >
+        {name}
+      </Box>
+      <Box
+        my={'auto'}
+        color={useColorModeValue('gray.800', 'gray.200')}
+        alignContent={'center'}>
+        <Avatar size="xl" src={image} alt="Avatar Alt" mb={4} pos="relative" />
+      </Box>
+      <Button colorScheme="teal" mt="5" size="sm">
+      ir
+      </Button>
+    </Box>
+  </Box>
   );
 };
 
