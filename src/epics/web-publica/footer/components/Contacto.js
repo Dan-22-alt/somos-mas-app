c553eb5f17dbace69353929b59899a38da3c10bd
Elyelin Carrasquero <elyelin15@gmail.com>
2021-07-27 21:29:31 -0300

    fixes

--- src/epics/web-publica/footer/components/Contacto.js
+++ src/epics/web-publica/footer/components/Contacto.js
@@ -1,27 +1,27 @@
-import { Stack, Text } from '@chakra-ui/react';
+import { Flex, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
-import FormNewsletter from './FormNewsletter';
 
 const Contactos = ({ titulo, organization }) => {
   return (
-    <Stack align={'center'} mx={'auto'} mt={0}>
-      <Text fontWeight="bold" fontSize={['md', 'lg', 'xl']} mb={2}>
-        {titulo}
-      </Text>
-      <Text fontSize={['sm', 'md', 'lg', 'xl']} align={'center'}>
-        {' '}
-        Teléfono: {organization.phone}
-      </Text>
-      <Text fontSize={['sm', 'md', 'lg', 'xl']} align={'center'}>
-        {' '}
-        Celular: {organization.cellphone}
-      </Text>
-      <Text fontSize={['sm', 'md', 'lg', 'xl']} align={'center'}>
-        {' '}
-        {organization.address}
-      </Text>
-      <FormNewsletter />
-    </Stack>
+    <Flex>
+      <Stack align={'center'} mx={'auto'} mt={0}>
+        <Text fontWeight="bold" fontSize={['md', 'lg', 'xl']} mb={2}>
+          {titulo}
+        </Text>
+        <Text fontSize={['sm', 'md', 'lg', 'xl']} align={'center'}>
+          {' '}
+          Teléfono: {organization.phone}
+        </Text>
+        <Text fontSize={['sm', 'md', 'lg', 'xl']} align={'center'}>
+          {' '}
+          Celular: {organization.cellphone}
+        </Text>
+        <Text fontSize={['sm', 'md', 'lg', 'xl']} align={'center'}>
+          {' '}
+          {organization.address}
+        </Text>
+      </Stack>
+    </Flex>
   );
 };
 
