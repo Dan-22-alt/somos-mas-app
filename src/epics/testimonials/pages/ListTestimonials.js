976a80cabc5dd7311e8d66ce9553eccb0a51f3d6
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-07-31 19:36:42 -0300

    connet with backoffice testimonials

--- src/epics/testimonials/pages/ListTestimonials.js
+++ src/epics/testimonials/pages/ListTestimonials.js
@@ -1,20 +1,28 @@
-import { Button, Container, SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
+import { Button, Container, SimpleGrid } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import TestimonialCard from '../components/TestimonialCard';
-import data from './testimonialsMockData';
+import { ConditionalRender } from '../../../components/Error/ErrorMsg';
 
 const ListTestimonials = () => {
+  const { testimonials } = useSelector(state => state)
+
+  const List = () => (
+    <SimpleGrid columns={{ lg: 3, sm: 2, base: 1 }} spacing={4}>
+      {testimonials.data.map( testimonial =>
+        <TestimonialCard key={testimonial.id} {...testimonial} />
+      )}
+    </SimpleGrid>
+  )
+
   return (
     <Container maxWidth="1024px" my={10}>
       <Link to="/backoffice/testimonials/create">
         <Button colorScheme="teal">Crear testimonio</Button>
       </Link>
-      <SimpleGrid columns={{ lg: 3, sm: 2, base: 1 }} spacing={4}>
-        {data.map((testimonial) => (
-          <TestimonialCard key={testimonial.id} {...testimonial} />
-        ))}
-      </SimpleGrid>
+      {ConditionalRender(testimonials.status, List, 'hola')}
     </Container>
   );
 };
